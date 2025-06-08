b7c817f5ab561bbf45bb6fe7ddfa4270370b4303
xdguy <done@xd.com>
2025-06-08 15:01:07 +0300

    commit

--- public static void main(String[] args) {.java
+++
@@ -1,9 +0,0 @@
-public static void main(String[] args) {
-          Integer value = 1;
-          change(value);
-          System.out.println(value);
-}
-
-public static void change(Integer value) {
-           ++value;
-}
